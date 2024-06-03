4f583fd832a79d63081c4529cf9a9ddf69ec2052
Георгий Хабнер <georgijhabner@Noutbuk-Georgij.local>
2024-06-03 06:19:43 +0300

    add some fixes

--- test.py
+++
@@ -1,8 +0,0 @@
-from src.isa import ArithmeticInstructionImm, Opcode, Registers
-
-with open("examples/prob1.bin", "rb") as f:
-    data = f.read()
-
-    for i in range(0, len(data), 2):
-        instr_data = data[i:i + 2]
-        print("".join(f"{byte:08b}" for byte in instr_data))
